6affa679e587fdc4b475a03138e78b45aae070db
Ethan Bundy <gunderodd@hotmail.com>
2019-10-27 06:46:12 -0700

    page layout, menu for each panel, audio for each player

--- js/click_trigger.js
+++ js/click_trigger.js
@@ -1,9 +1,119 @@
-function toggleMenu() {
-  var menuBox = document.getElementById('menu-box');
-  if(menuBox.style.display == "grid") { // if is menuBox displayed, hide it
-    menuBox.style.display = "none";
+// introduction menu box
+
+function toggleMenuI() {
+  var menuBoxI = document.getElementById('menu-box-i');
+  if(menuBoxI.style.display == "grid") { // if is menuBox displayed, hide it
+    menuBoxI.style.display = "none";
   }
   else { // if is menuBox hidden, display it
-    menuBox.style.display = "grid";
+    menuBoxI.style.display = "grid";
+  }
+
+  var menuImageI = document.getElementById('menu-image-i');
+  if(menuImageI.style.display == "block") { // if is menuBox displayed, hide it
+    menuImageI.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuImageI.style.display = "block";
+  }
+}
+
+// first page menu box
+
+function toggleMenu1() {
+  var menuBox1 = document.getElementById('menu-box-1');
+  if(menuBox1.style.display == "grid") { // if is menuBox displayed, hide it
+    menuBox1.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuBox1.style.display = "grid";
+  }
+
+  var menuImage1 = document.getElementById('menu-image-1');
+  if(menuImage1.style.display == "block") { // if is menuBox displayed, hide it
+    menuImage1.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuImage1.style.display = "block";
+  }
+}
+
+// second page menu box
+
+function toggleMenu2() {
+  var menuBox2 = document.getElementById('menu-box-2');
+  if(menuBox2.style.display == "grid") { // if is menuBox displayed, hide it
+    menuBox2.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuBox2.style.display = "grid";
+  }
+
+  var menuImage2 = document.getElementById('menu-image-2');
+  if(menuImage2.style.display == "block") { // if is menuBox displayed, hide it
+    menuImage2.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuImage2.style.display = "block";
+  }
+}
+
+// third page menu box
+
+function toggleMenu3() {
+  var menuBox3 = document.getElementById('menu-box-3');
+  if(menuBox3.style.display == "grid") { // if is menuBox displayed, hide it
+    menuBox3.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuBox3.style.display = "grid";
+  }
+
+  var menuImage3 = document.getElementById('menu-image-3');
+  if(menuImage3.style.display == "block") { // if is menuBox displayed, hide it
+    menuImage3.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuImage3.style.display = "block";
+  }
+}
+
+// fourth page menu box
+
+function toggleMenu4() {
+  var menuBox4 = document.getElementById('menu-box-4');
+  if(menuBox4.style.display == "grid") { // if is menuBox displayed, hide it
+    menuBox4.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuBox4.style.display = "grid";
+  }
+
+  var menuImage4 = document.getElementById('menu-image-4');
+  if(menuImage4.style.display == "block") { // if is menuBox displayed, hide it
+    menuImage4.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuImage4.style.display = "block";
+  }
+}
+
+// fifth page menu box
+
+function toggleMenu5() {
+  var menuBox5 = document.getElementById('menu-box-5');
+  if(menuBox5.style.display == "grid") { // if is menuBox displayed, hide it
+    menuBox5.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuBox5.style.display = "grid";
+  }
+
+  var menuImage5 = document.getElementById('menu-image-5');
+  if(menuImage5.style.display == "block") { // if is menuBox displayed, hide it
+    menuImage5.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuImage5.style.display = "block";
   }
 }
